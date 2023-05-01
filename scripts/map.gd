f3d292ded31c42c06fe50c2cadfd83bc234e44ae
TitouanCh <titouan.azert@wadsa.com>
2023-05-01 22:47:36 +0200

    grass

--- scripts/map.gd
+++ scripts/map.gd
@@ -3,8 +3,9 @@ extends Node3D
 var camera
 var dimension = 600
 
-@onready var mesh = $mesh
+@onready var continent = $continent
 @onready var ocean = $ocean
+@onready var grass = $grass
 
 func _ready():
 	if get_parent():
@@ -12,8 +13,9 @@ func _ready():
 
 func _process(delta):
 	if camera:
-		mesh.mesh.material.set_shader_parameter("position", Vector2(camera.position.x, camera.position.z))
-		ocean.mesh.material.set_shader_parameter("position", Vector2(camera.position.x, camera.position.z))
-#		
+		RenderingServer.global_shader_parameter_set("position", Vector2(camera.position.x, camera.position.z))
+#		continent.mesh.material.set_shader_parameter("position", Vector2(camera.position.x, camera.position.z))
+#		ocean.mesh.material.set_shader_parameter("position", Vector2(camera.position.x, camera.position.z))
+#		grass.process_material.set_shader_parameter("position", Vector2(camera.position.x, camera.position.z))
 #		mesh.mesh.material.set_shader_parameter("mouse_position", Vector2(camera.mouse_projection.x/2 + 0.5, camera.mouse_projection.z/2 + 0.5))
 	
