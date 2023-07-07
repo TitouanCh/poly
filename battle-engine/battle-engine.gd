307ddc63213dd1164915c1ee7f7fd8577889de53
TitouanCh <titouan.azert@wadsa.com>
2023-07-07 15:09:58 +0200

    combat engine movement

--- battle-engine/battle-engine.gd
+++ battle-engine/battle-engine.gd
@@ -9,11 +9,12 @@ func _ready():
 #	var test2 = Unit.new()
 	test = Constants.unit_compendium[0].clone()
 	test.change_position(Vector2(200, 200), 0)
-	
+	test.target_positions = test.place_soldiers(Vector2(800, 800), 180)
+	print(test.target_positions)
 	print(test.soldiers)
 
 func _process(delta):
-#	print(Constants.unit_compendium)
+	test.move(delta * 8)
 	queue_redraw()
 
 func _draw():
@@ -21,5 +22,5 @@ func _draw():
 
 func draw_unit(unit: Unit):
 	for i in range(len(unit.soldiers)):
-		draw_circle(unit.positions[i] + unit.position, 4, Color(255, 255, 255))
-	draw_circle(unit.position, 3, Color(255, 0, 0))	
+		draw_circle(unit.positions[i], 2, Color(255, 255, 255))
+	#draw_circle(unit.position, 3, Color(255, 0, 0))	
--- battle-engine/unit_class.gd
+++ battle-engine/unit_class.gd
@@ -4,8 +4,9 @@ class_name Unit
 
 var name = "Default"
 var soldiers = [0, 0, 0, 0]
-var position: Vector2 = Vector2.ZERO
 var positions = []
+var target_positions = []
+var speed: float = 8
 var n: int
 var spacing: float
 var width: int
@@ -24,14 +25,25 @@ func clone():
 	return Unit.new().setup(name, soldiers, spacing, width, stance)
 
 func change_position(_position, _angle):
-	position = _position
-	positions = place_soldiers(_angle)
+	positions = place_soldiers(_position, _angle)
 
-func place_soldiers(unit_angle: float) -> Array:
+func place_soldiers(_position: Vector2, unit_angle: float = 0.0) -> Array:
 	var soldier_positions = []
-	print((width - 1)/2)
+
 	for i in range(n):
+		# Offset
 		soldier_positions.append((Vector2(i % width, floor(i / width)) 
 		- Vector2((float(width) - 1)/2, (ceil(float(n)/float(width)) - 1)/2)) * spacing)
+		
+		# Rotation
+		soldier_positions[i] = soldier_positions[i].rotated(unit_angle)
+		
+		# Position
+		soldier_positions[i] += _position
 	
 	return soldier_positions
+
+func move(delta):
+	for i in range(len(soldiers)):
+		var direction = (target_positions[i] - positions[i]).normalized()
+		positions[i] += direction * speed * delta
