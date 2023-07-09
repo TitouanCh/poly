5e55eaef486952e6be325911c7eaffc26d239c91
TitouanCh <titouan.azert@wadsa.com>
2023-07-09 16:15:52 +0200

    combat engine: basic combat structure

--- battle-engine/battle-engine.gd
+++ battle-engine/battle-engine.gd
@@ -2,24 +2,92 @@ extends Node2D
 
 var units = []
 var base_mouse_position = null
+var combat_range = 300
 
-func engine_process(detla, list_of_units):
-	pass
+func engine_process(delta, units):
+	for i in range(len(units)):
+		# Move unit
+		units[i].move(delta)
+		# Check if enemies are in range
+		units[i].incombat = false
+		for j in range(i + 1, len(units)):
+			if units[i].team != units[j].team:
+				if units[i].center_of_mass.distance_to(units[j].center_of_mass) < combat_range:
+#					# Attack
+					combat(delta, units[i], units[j])
+					units[i].incombat = true
+		# If incombat, add time to combat timer
+		if units[i].incombat:
+			units[i].incombat_timer += delta
+
+func combat(delta, unit1: Unit, unit2: Unit):
+	var max_width_unit = unit1
+	var min_width_unit = unit2
+	if unit2.width - unit2.soldiers_incombat > unit1.width - unit1.soldiers_incombat:
+		max_width_unit = unit2
+		min_width_unit = unit1
+	
+	var combatants = max_width_unit.soldiers_incombat
+	
+	for i in range(len(min_width_unit.PStype)):
+		if combatants == max_width_unit.width:
+			break
+		
+		if !min_width_unit.PSincombat[i]:
+			for j in range(len(max_width_unit.PStype)):
+				if !max_width_unit.PSincombat[j]:
+					min_width_unit.PSincombat[i] = true
+					max_width_unit.PSincombat[j] = true
+					min_width_unit.soldiers_incombat += 1
+					max_width_unit.soldiers_incombat += 1
+					combatants += 1
+					
+					var combat_positions = get_combat_positions(min_width_unit.PSposition[i], max_width_unit.PSposition[j])
+					min_width_unit.PScombat_position[i] = combat_positions[0]
+					max_width_unit.PScombat_position[j] = combat_positions[1]
+					break
+
+func get_combat_positions(soldier1_position: Vector2, soldier2_position: Vector2):
+	var avg = (soldier1_position + soldier2_position)/2
+	return [avg, avg]
 
 func _ready():
-	create_unit(0, Vector2(200, 200), 0)
-	create_unit(0, Vector2(400, 200), 0)
+#	# PERFORMANCE TEST
+#	for i in range(100):
+#		create_unit(0, Vector2(1000 * randf(), 1000 * randf()), 0)
+	
+	create_unit(0, Vector2(800, 200), 0, 1)
+	create_unit(0, Vector2(200, 200), 0, 2)
 
 func _process(delta):
-	for unit in units:
-		unit.move(delta)
+	engine_process(delta, units)
+	
+	# Debug ? Inputs --- 
+	var mouse_position = get_viewport().get_mouse_position()
+	if Input.is_action_just_released("left_click"):
+		var action_performed = false
+		# Check for select action
+		for unit in units:
+			if mouse_position.distance_to(unit.center_of_mass) < 12:
+				unit.selected = !unit.selected
+				# Deselect other units
+				if unit.selected: for _unit in units: if _unit != unit: _unit.selected = false
+				action_performed = true
+		# Check for movement action
+		if !action_performed:
+			for unit in units:
+				if unit.selected:
+					unit.orders.append(["r", (mouse_position - base_mouse_position).angle() + PI/2])
+					unit.orders.append(["g", base_mouse_position])
+		base_mouse_position = null
+	
 	queue_redraw()
 
 func _draw():
 	for unit in units:
 		draw_unit(unit)
 	
-	# Inputs --- 
+	# Debug ? Unit indicators
 	var mouse_position = get_viewport().get_mouse_position()
 	if Input.is_action_pressed("left_click"):
 		if base_mouse_position == null:
@@ -35,31 +103,16 @@ func _draw():
 		for unit in units:
 			if unit.selected:
 				draw_unit(unit.preview_at(base_mouse_position, angle + PI/2), true)
-	if Input.is_action_just_released("left_click"):
-		var action_performed = false
-		# Check for select action
-		for unit in units:
-			if mouse_position.distance_to(unit.center_of_mass) < 12:
-				unit.selected = !unit.selected
-				# Deselect other units
-				if unit.selected: for _unit in units: if _unit != unit: _unit.selected = false
-				action_performed = true
-		# Check for movement action
-		if !action_performed:
-			for unit in units:
-				if unit.selected:
-					unit.orders.append(["r", (get_viewport().get_mouse_position() - base_mouse_position).angle() + PI/2])
-					unit.orders.append(["g", base_mouse_position])
-		base_mouse_position = null
 
 func draw_unit(unit: Unit, preview = false):
-	for i in range(len(unit.soldiers)):
-		draw_circle(unit.positions[i], 2, Color(255 * int(!preview), 255, 255))
+	for i in range(len(unit.PStype)):
+		draw_circle(unit.PSposition[i], 2, Color(255 * int(!preview), 255, 255))
 	draw_circle(unit.center_of_mass, 3, Color(int(!unit.selected) * 255, int(unit.selected) * 255, 0))	
 
-func create_unit(compendium_idx, _position, _angle):
+func create_unit(compendium_idx, _position, _angle, _team):
 	var unit = Constants.create_unit_from(Constants.unit_compendium[compendium_idx])
 	unit.change_position(_position, _angle)
+	unit.team = _team
 	units.append(unit)
 #	unit.target_positions = unit.place_soldiers(Vector2(800, 800), 180)
 	
--- battle-engine/unit_class.gd
+++ battle-engine/unit_class.gd
@@ -2,38 +2,56 @@ extends Object
 
 class_name Unit
 
+# Unit type specific info
 var name = "Default"
-var soldiers = [0, 0, 0, 0]
-var positions = []
-var center_of_mass: Vector2 = Vector2.ZERO
-var target_positions = []
-var orders = []
+var stance: String = "regular"
 var speed: float = 100
-var current_angle = 0
-var current_position = Vector2.ZERO
-var n: int
 var spacing: float
 var width: int
-var stance: String = "regular"
+var n: int
+
+# Realtime
+var center_of_mass: Vector2 = Vector2.ZERO
+var current_angle = 0
+var current_position = Vector2.ZERO
+var team: int = 0
+var incombat = false
+
+# Per soldier info
+var PStype = [0, 0, 0, 0]
+var PSposition = []
+var PStarget_position = []
+var PScombat_position = []
+var PSincombat = []
+
+
+var orders = []
+
+# Combat
+var incombat_timer = 0
+var soldiers_incombat = 0
 
 # DEBUG --
 var selected = false
 
-func setup(_name, _soldiers, _spacing, _width, _stance = "regular"):
+func setup(_name, _PStype, _spacing, _width, _stance = "regular"):
 	name = _name
-	soldiers = _soldiers
+	PStype = _PStype
 	spacing = _spacing
 	width = _width
 	stance = _stance
-	n = len(soldiers)
+	n = len(PStype)
+	for i in range(n): PSincombat.append(false)
 	return self
 
 func clone():
-	return Unit.new().setup(name, soldiers, spacing, width, stance)
+	return Unit.new().setup(name, PStype, spacing, width, stance)
 
 func change_position(_position, _angle):
-	positions = place_soldiers(_position, _angle)
-	target_positions = positions
+	PSposition = place_soldiers(_position, _angle)
+	PScombat_position = PSposition.duplicate()
+	center_of_mass = _position
+	PStarget_position = PSposition
 
 func place_soldiers(_position: Vector2, unit_angle: float = 0.0) -> Array:
 	var soldier_positions = []
@@ -54,24 +72,34 @@ func place_soldiers(_position: Vector2, unit_angle: float = 0.0) -> Array:
 func move(delta):
 	var sum = Vector2.ZERO
 	var deaccel_epsilon = 50
-	for i in range(len(soldiers)):
-		var direction = target_positions[i] - positions[i]
-		var distance = direction.length()
-		direction = direction.normalized()
-		var speed_mod = speed
-		if distance < deaccel_epsilon:
-			speed_mod = max(lerp(0.0, speed, distance/deaccel_epsilon), 1.0)
-		positions[i] += direction * speed_mod * delta
-		sum += positions[i]
-	center_of_mass = sum/len(soldiers)
+	for i in range(len(PStype)):
+		if !PSincombat[i]:
+			var direction = PStarget_position[i] - PSposition[i]
+			var distance = direction.length()
+			direction = direction.normalized()
+			var speed_mod = speed
+			if distance < deaccel_epsilon:
+				speed_mod = max(lerp(0.0, speed, distance/deaccel_epsilon), 1.0)
+			PSposition[i] += direction * speed_mod * delta
+			sum += PSposition[i]
+		else:
+			var direction = PScombat_position[i] - PSposition[i]
+			var distance = direction.length()
+			direction = direction.normalized()
+			var speed_mod = speed
+			if distance < deaccel_epsilon:
+				speed_mod = max(lerp(0.0, speed, distance/deaccel_epsilon), 1.0)
+			PSposition[i] += direction * speed_mod * delta
+	center_of_mass = sum/(len(PStype) - soldiers_incombat)
 	
 	order_check(delta)
 
 func order_check(delta):
 	var order_epsilon = 20
 	var sum = 0
-	for i in range(len(positions)):
-		sum += positions[i].distance_to(target_positions[i])
+	for i in range(len(PSposition)):
+		if !PSincombat[i]:
+			sum += PSposition[i].distance_to(PStarget_position[i])
 	
 	if sum < order_epsilon:
 #		print("NEXT ORDER!!!")
@@ -89,10 +117,10 @@ func queue_next_order():
 		if len(orders) > 0:
 			# Go order
 			if orders[0][0] == "g":
-				target_positions = place_soldiers(orders[0][1], current_angle)
+				PStarget_position = place_soldiers(orders[0][1], current_angle)
 			# Rotate order
 			if orders[0][0] == "r":
-				target_positions = place_soldiers(current_position, orders[0][1])
+				PStarget_position = place_soldiers(current_position, orders[0][1])
 
 # DEBUG --
 func preview_at(_position, _angle) -> Unit:
